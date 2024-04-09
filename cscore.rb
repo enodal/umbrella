60d5328d8ff1ba00416b9d97604a15538384e250
enodal <164991691+enodal@users.noreply.github.com>
2024-04-09 23:53:31 +0000

    next

--- cscore.rb
+++ cscore.rb
@@ -5,12 +5,17 @@ line_width = 40
 
 puts "Which university data would you like to view?"
 
-uni_name = gets.chomp
-
+#uni_name = gets.chomp.gsub(" ","%20")
+uni_name = "University%20of%20Chicago"
 pp uni_name
+
 
 cscore_key = ENV.fetch("CSCORE")
 
-cscore_url = https://api.data.gov/ed/collegescorecard/v1/schools.json?
+cscore_url = "https://api.data.gov/ed/collegescorecard/v1/schools.json?api_key=#{cscore_key}school.name=#{uni_name}"
 
 pp cscore_url
+
+raw_response = HTTP.get(cscore_url)
+
+parsed_url = JSON.parse(raw_response)
