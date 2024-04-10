e157e64439360c7c7f925387ea55b0672444b666
enodal <164991691+enodal@users.noreply.github.com>
2024-04-10 00:55:02 +0000

    Chicken

--- cscore.rb
+++ cscore.rb
@@ -12,10 +12,34 @@ pp uni_name
 
 cscore_key = ENV.fetch("CSCORE")
 
-cscore_url = "https://api.data.gov/ed/collegescorecard/v1/schools.json?api_key=#{cscore_key}school.name=#{uni_name}"
+cscore_url = "https://api.data.gov/ed/collegescorecard/v1/schools.json?api_key=#{cscore_key}&school.name=#{uni_name}"
 
 pp cscore_url
 
 raw_response = HTTP.get(cscore_url)
 
 parsed_url = JSON.parse(raw_response)
+results = parsed_url.fetch("results")
+
+results.each do |name|
+latest = name.fetch("latest")
+school = latest.fetch("school")
+uni = school.fetch("name")
+if uni == uni_name.gsub("%20", " ")
+  uni_data = latest
+  programs = uni_data.fetch("programs")
+  cip = programs.fetch("cip_4_digit")
+  pp "What Major?"
+  what_major = gets.chomp.titleize + "."
+  pp what_major
+  cip.each do |title|
+    major = title.fetch("title")
+    credential = title.fetch("credential")
+    level = credential.fetch("title")
+    if level == "Bachelor's Degree" &&
+      major == what_major
+      pp "lfg"
+    end
+  end
+end
+end
