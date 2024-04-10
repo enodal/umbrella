50e2029f1e404bc3bd58ea6479638f4b953412c8
enodal <164991691+enodal@users.noreply.github.com>
2024-04-10 04:33:50 +0000

    error with south carolina

--- cscore.rb
+++ cscore.rb
@@ -5,27 +5,28 @@ line_width = 40
 
 puts "Which university data would you like to view?"
 
-#uni_name = gets.chomp.gsub(" ","%20")
-uni_name = "University%20of%20Chicago"
+uni_name = gets.chomp.gsub(" ","%20")
+#uni_name = "University%20of%20Chicago"
 pp uni_name
 
 
 cscore_key = ENV.fetch("CSCORE")
 
-cscore_url = "https://api.data.gov/ed/collegescorecard/v1/schools.json?api_key=#{cscore_key}&school.name=#{uni_name}"
+cscore_url = "https://api.data.gov/ed/collegescorecard/v1/schools.json?api_key=#{cscore_key}&school.name=#{uni_name}&per_page=100"
 
 pp cscore_url
 
 raw_response = HTTP.get(cscore_url)
 
 parsed_url = JSON.parse(raw_response)
+
 results = parsed_url.fetch("results")
 
 results.each do |name|
 latest = name.fetch("latest")
 school = latest.fetch("school")
 uni = school.fetch("name")
-if uni == uni_name.gsub("%20", " ")
+if uni == uni_name.gsub("%20"," ")
   uni_data = latest
   programs = uni_data.fetch("programs")
   cip = programs.fetch("cip_4_digit")
@@ -38,7 +39,10 @@ if uni == uni_name.gsub("%20", " ")
     level = credential.fetch("title")
     if level == "Bachelor's Degree" &&
       major.upcase == what_major
-      
+      earnings = title.fetch("earnings")
+      four_yr = earnings.fetch("4_yr")
+      med_earnings = four_yr.fetch("overall_median_earnings")
+      pp "The overall median earnings for someone with a #{what_major.capitalize.gsub(".","")} degree at #{uni_name.gsub("%20"," ")} is $#{med_earnings}"
     end
   end
 end
